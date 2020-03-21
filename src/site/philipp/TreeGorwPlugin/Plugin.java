c3c65e25a2042efc33b1ffd4f6f614cd88ac6226
P100sch <ph.100sch@gmail.com>
2020-03-22 00:19:35 +0100

    Spigot Setup

---
+++ src/site/philipp/TreeGorwPlugin/Plugin.java
@@ -0,0 +1,14 @@
+package site.philipp.TreeGorwPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
