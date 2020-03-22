adab19b76d4fbb31541c2835f75e1cbdd0dfb3a4
P100sch <ph.100sch@gmail.com>
2020-03-22 01:31:27 +0100

    Done for now

--- src/site/philipp/TreeGorwPlugin/Plugin.java
+++ src/site/philipp/TreeGorwPlugin/Plugin.java
@@ -1,14 +1,59 @@
 package site.philipp.TreeGorwPlugin;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.Sapling;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventException;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.plugin.EventExecutor;
+import org.bukkit.plugin.RegisteredListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin {
+    private RegisteredListener toggleSneakListener;
+
+    public Plugin() {
+        toggleSneakListener = new RegisteredListener(new Listener() {
+        }, new EventExecutor() {
+            @Override
+            public void execute(Listener listener, Event event) throws EventException {
+                PlayerToggleSneakEvent sneakEvent = (PlayerToggleSneakEvent) event;
+                World currentWorld = sneakEvent.getPlayer().getWorld();
+                Location playerLocation = sneakEvent.getPlayer().getLocation();
+                for (int x = playerLocation.getBlockX() - 2; x <= playerLocation.getBlockX() + 2; x++) {
+                    if (x == playerLocation.getBlockX()) {
+                        continue;
+                    }
+                    for (int y = playerLocation.getBlockY() - 2; y <= playerLocation.getBlockY() + 2; y++) {
+                        if (y == playerLocation.getBlockY()) {
+                            continue;
+                        }
+                        for (int z = playerLocation.getBlockZ() - 2; z <= playerLocation.getBlockZ() + 2; z++) {
+                            if (z == playerLocation.getBlockZ()) {
+                                continue;
+                            }
+                            BlockData blockData = currentWorld.getBlockAt(x, y, z).getBlockData();
+                            if (blockData instanceof Sapling) {
+                                Sapling sapling = ((Sapling) blockData);
+                                sapling.setStage(sapling.getMaximumStage());
+                            }
+                        }
+                    }
+                }
+            }
+        }, EventPriority.LOWEST, this, false);
+    }
+
     @Override
     public void onEnable(){
-        //Fired when the server enables the plugin
+        PlayerToggleSneakEvent.getHandlerList().register(toggleSneakListener);
     }
     @Override
     public void onDisable(){
-        //Fired when the server stops and disables all plugins
+        PlayerToggleSneakEvent.getHandlerList().unregister(toggleSneakListener);
     }
 }
